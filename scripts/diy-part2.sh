dac8d37c396130e8db940adbbce37af03cbbf4d0
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-06-27 19:09:09 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/192.168.1./192.168.0./g' .config
+
 sed -i 's/CONFIG_TARGET_MULTI_PROFILE=y/CONFIG_TARGET_MULTI_PROFILE=n/' .config
 sed -i '/CONFIG_TARGET_DEVICE_/d' .config
 echo "CONFIG_TARGET_mediatek_filogic_DEVICE_cmcc_rax3000m=y" >> .config
