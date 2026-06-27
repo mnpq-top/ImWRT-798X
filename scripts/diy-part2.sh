c8d3adb20f25ccbdf76d837cc6ced3b4a8a9d43c
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-06-28 03:08:32 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -7,20 +7,19 @@ echo "CONFIG_TARGET_mediatek_filogic_DEVICE_cmcc_rax3000m=y" >> .config
 cat <<EOF >> .config
 
 CONFIG_PACKAGE_nano=y
-#CONFIG_PACKAGE_cfdisk=y
+CONFIG_PACKAGE_cfdisk=m
 CONFIG_PACKAGE_kmod-tls=y
-#CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=m
 
-CONFIG_PACKAGE_docker=y
-CONFIG_PACKAGE_docker-compose=y
-CONFIG_PACKAGE_dockerd=y
+CONFIG_PACKAGE_docker=m
+CONFIG_PACKAGE_docker-compose=m
+CONFIG_PACKAGE_dockerd=m
 
 CONFIG_PACKAGE_luci-proto-wireguard=y
 
 CONFIG_PACKAGE_mosquitto-ssl=y
 
 CONFIG_PACKAGE_adguardhome=y
-#CONFIG_PACKAGE_xray-core=y
 
 CONFIG_PACKAGE_cloudflared=y
 CONFIG_PACKAGE_luci-app-cloudflared=y
@@ -30,8 +29,8 @@ CONFIG_PACKAGE_luci-app-samba4=y
 CONFIG_PACKAGE_luci-i18n-samba4-zh-cn=y
 
 CONFIG_PACKAGE_rtp2httpd=y
-#CONFIG_PACKAGE_luci-app-rtp2httpd=y
-#CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
+CONFIG_PACKAGE_luci-app-rtp2httpd=y
+CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
 
 CONFIG_PACKAGE_luci-app-arpbind=y
 CONFIG_PACKAGE_luci-i18n-arpbind-zh-cn=y
@@ -39,7 +38,7 @@ CONFIG_PACKAGE_luci-i18n-arpbind-zh-cn=y
 CONFIG_PACKAGE_luci-app-wol=y
 CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
 
-#CONFIG_PACKAGE_luci-app-zerotier=y
-#CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y
+CONFIG_PACKAGE_luci-app-zerotier=m
+CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=m
 
 EOF
