0c92203401d0a8ba13736c4ad1c81e666c8a5a39
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-06-28 03:31:23 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -7,13 +7,13 @@ echo "CONFIG_TARGET_mediatek_filogic_DEVICE_cmcc_rax3000m=y" >> .config
 cat <<EOF >> .config
 
 CONFIG_PACKAGE_nano=y
-CONFIG_PACKAGE_cfdisk=m
+CONFIG_PACKAGE_cfdisk=y
 CONFIG_PACKAGE_kmod-tls=y
 CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
 
-CONFIG_PACKAGE_docker=m
-CONFIG_PACKAGE_docker-compose=m
-CONFIG_PACKAGE_dockerd=m
+CONFIG_PACKAGE_docker=y
+CONFIG_PACKAGE_docker-compose=y
+CONFIG_PACKAGE_dockerd=y
 
 CONFIG_PACKAGE_luci-proto-wireguard=y
 
@@ -35,7 +35,7 @@ CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
 CONFIG_PACKAGE_luci-app-wol=y
 CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
 
-CONFIG_PACKAGE_luci-app-zerotier=m
-CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=m
+#CONFIG_PACKAGE_luci-app-zerotier=m
+#CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=m
 
 EOF
