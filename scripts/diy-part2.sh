3f92d9afdf8281a746f52dbe85c248d86e1e9d02
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-06-28 08:40:58 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -9,7 +9,7 @@ cat <<EOF >> .config
 CONFIG_PACKAGE_nano=y
 CONFIG_PACKAGE_cfdisk=y
 CONFIG_PACKAGE_kmod-tls=y
-CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
+#CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
 
 CONFIG_PACKAGE_docker=y
 CONFIG_PACKAGE_docker-compose=y
@@ -34,8 +34,5 @@ CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
 
 CONFIG_PACKAGE_luci-app-wol=y
 CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
-
-#CONFIG_PACKAGE_luci-app-zerotier=m
-#CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=m
 
 EOF
