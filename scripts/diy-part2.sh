d743f67602ad92ab8fc5f46f806d2834a729fbcc
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-06-30 03:58:46 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,9 +10,9 @@ CONFIG_PACKAGE_nano=y
 #CONFIG_PACKAGE_cfdisk=y
 CONFIG_PACKAGE_kmod-tls=y
 
-#CONFIG_PACKAGE_kmod-usb-net=y
-#CONFIG_PACKAGE_kmod-usb-net-rndis=y
-#CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
+CONFIG_PACKAGE_kmod-usb-net=y
+CONFIG_PACKAGE_kmod-usb-net-rndis=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
 
 CONFIG_PACKAGE_docker=y
 CONFIG_PACKAGE_docker-compose=y
@@ -20,7 +20,7 @@ CONFIG_PACKAGE_dockerd=y
 
 CONFIG_PACKAGE_luci-proto-wireguard=y
 
-#CONFIG_PACKAGE_mosquitto-ssl=y
+CONFIG_PACKAGE_mosquitto-ssl=y
 
 #CONFIG_PACKAGE_adguardhome=y
 CONFIG_PACKAGE_https-dns-proxy=y
@@ -31,20 +31,20 @@ CONFIG_PACKAGE_cloudflared=y
 CONFIG_PACKAGE_luci-app-cloudflared=y
 CONFIG_PACKAGE_luci-i18n-cloudflared-zh-cn=y
 
-#CONFIG_PACKAGE_luci-app-samba4=y
-#CONFIG_PACKAGE_luci-i18n-samba4-zh-cn=y
+CONFIG_PACKAGE_luci-app-samba4=y
+CONFIG_PACKAGE_luci-i18n-samba4-zh-cn=y
 
-#CONFIG_PACKAGE_rtp2httpd=y
-#CONFIG_PACKAGE_luci-app-rtp2httpd=y
-#CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
+CONFIG_PACKAGE_rtp2httpd=y
+CONFIG_PACKAGE_luci-app-rtp2httpd=y
+CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y
 
-#CONFIG_PACKAGE_luci-app-wol=y
-#CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
+CONFIG_PACKAGE_luci-app-wol=y
+CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
 
 CONFIG_PACKAGE_luci-app-eqos-mtk=y
 CONFIG_PACKAGE_luci-i18n-eqos-mtk-zh-cn=y
 
-#CONFIG_PACKAGE_luci-app-nlbwmon=y
-#CONFIG_PACKAGE_luci-i18n-nlbwmon-zh-cn=y
+CONFIG_PACKAGE_luci-app-nlbwmon=y
+CONFIG_PACKAGE_luci-i18n-nlbwmon-zh-cn=y
 
 EOF
