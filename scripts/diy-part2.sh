5e505623d4510a8a06e65b4a2d564ebb614be8a5
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-08-15 12:40:27 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -14,6 +14,7 @@ CONFIG_PACKAGE_kmod-tls=y
 #CONFIG_PACKAGE_kmod-ipt-nat6=y
 #CONFIG_PACKAGE_kmod-bonding=y
 #CONFIG_PACKAGE_luci-app-bonding=y
+CONFIG_PACKAGE_luci-proto-wireguard=y
 
 CONFIG_PACKAGE_kmod-usb2=y
 CONFIG_PACKAGE_kmod-usb3=y
@@ -27,19 +28,13 @@ CONFIG_PACKAGE_docker=y
 CONFIG_PACKAGE_docker-compose=y
 CONFIG_PACKAGE_dockerd=y
 
-CONFIG_PACKAGE_luci-proto-wireguard=y
-
-#CONFIG_PACKAGE_adguardhome=y
 CONFIG_PACKAGE_https-dns-proxy=y
 CONFIG_PACKAGE_luci-app-https-dns-proxy=y
 CONFIG_PACKAGE_luci-i18n-https-dns-proxy-zh-cn=y
 
-#CONFIG_PACKAGE_cloudflared=y
-#CONFIG_PACKAGE_luci-app-cloudflared=y
-#CONFIG_PACKAGE_luci-i18n-cloudflared-zh-cn=y
-
-#CONFIG_PACKAGE_luci-app-wol=y
-#CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
+CONFIG_PACKAGE_cloudflared=y
+CONFIG_PACKAGE_luci-app-cloudflared=y
+CONFIG_PACKAGE_luci-i18n-cloudflared-zh-cn=y
 
 CONFIG_PACKAGE_luci-app-eqos-mtk=y
 CONFIG_PACKAGE_luci-i18n-eqos-mtk-zh-cn=y
