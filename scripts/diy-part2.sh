863b68675fb911ddbabf1084c89a9057b91c3536
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-08-15 12:36:32 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,8 +10,8 @@ CONFIG_PACKAGE_nano=y
 #CONFIG_PACKAGE_cfdisk=y
 CONFIG_PACKAGE_kmod-tls=y
 
-CONFIG_PACKAGE_kmod-nf-nat6=y
-CONFIG_PACKAGE_kmod-ipt-nat6=y
+#CONFIG_PACKAGE_kmod-nf-nat6=y
+#CONFIG_PACKAGE_kmod-ipt-nat6=y
 #CONFIG_PACKAGE_kmod-bonding=y
 #CONFIG_PACKAGE_luci-app-bonding=y
 
