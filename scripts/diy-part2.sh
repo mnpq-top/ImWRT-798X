4645a9bc168722d1dd2563e03c45b37114e59565
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2026-08-17 13:01:30 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -18,11 +18,11 @@ CONFIG_PACKAGE_luci-proto-wireguard=y
 
 CONFIG_PACKAGE_kmod-usb2=y
 CONFIG_PACKAGE_kmod-usb3=y
-CONFIG_PACKAGE_kmod-usb-net=y
-CONFIG_PACKAGE_kmod-usb-net-rndis=y
-CONFIG_PACKAGE_r8152-firmware=y
+#CONFIG_PACKAGE_kmod-usb-net=y
+#CONFIG_PACKAGE_kmod-usb-net-rndis=y
+#CONFIG_PACKAGE_r8152-firmware=y
 #CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
-CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
+#CONFIG_PACKAGE_kmod-usb-net-rtl8152-vendor=y
 
 CONFIG_PACKAGE_docker=y
 CONFIG_PACKAGE_docker-compose=y
